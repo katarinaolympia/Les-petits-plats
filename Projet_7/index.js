f08e088b544d2fab01749b4f8bac553809a4f63f
Katarina Olympia <bajickatarina.pro@icloud.com>
2022-11-13 20:59:33 +0100

    JS : recherche dynamique avec forEach()

--- Projet_7/index.js
+++ Projet_7/index.js
@@ -18,7 +18,7 @@ function displayRecipes(recipesArray) {
         <div class="recipe_card">
                     <a href="">
                         <img class="recipe_image" src="" alt="${recipes.name}">
-                        <h2>${recipes.name}</h2>
+                        <h2 id="recipes_name">${recipes.name}</h2>
                         <span class="title_aside">
                             <svg class="clock_icon" width="30" height="30" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM10 18C5.59 18 2 14.41 2 10C2 5.59 5.59 2 10 2C14.41 2 18 5.59 18 10C18 14.41 14.41 18 10 18ZM10.5 5H9V11L14.2 14.2L15 12.9L10.5 10.2V5Z" fill="black"/>
@@ -52,3 +52,30 @@ function displayRecipes(recipesArray) {
     
     document.querySelector(".recipe_container").innerHTML = html
 }
+
+// Méthode fonctionnelle de recherche de recettes
+
+const searchInput = document.querySelector("#search")
+
+searchInput.addEventListener("input", (e) => {
+    
+    const content = e.target.value.toLowerCase()
+    const cards = document.querySelectorAll('.recipe_card')
+
+    if(content.length > 2) {
+        cards.forEach((card) => {
+            if (card.textContent.toLowerCase().includes(content)) {
+                card.style.display = "block"
+            }
+            else {
+                card.style.display = "none"
+            }
+        })
+    }
+
+    if(content.length <= 2) {
+            cards.forEach((card) => {
+                card.style.display = "block"
+            })
+    }
+})
