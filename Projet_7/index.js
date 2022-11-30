90d6c512c60ce64d3204e6afb6bf57167f07b576
Katarina Olympia <bajickatarina.pro@icloud.com>
2022-11-30 20:00:19 +0100

    JS : recherche dynamique en programmation fonctionnelle, filtre de dropdown (en cours)

--- Projet_7/index.js
+++ Projet_7/index.js
@@ -1,81 +1,79 @@
-fetch("./recipes.json")
-.then((response) => {
-    return response.json()
-})
-.then((data) => {
-    const recipes = data.recipes
-  
-    displayRecipes(recipes)
-})
+displayRecipes(recipes)
+getIngredients(recipes)
+// displayIngredients(recipes)
 
 
-// Création du DOM
-
-function displayRecipes(recipesArray) {
-    let html = ""
-    recipesArray.forEach((recipes) => {
-        html += `
-        <div class="recipe_card">
-                    <a href="">
-                        <img class="recipe_image" src="" alt="${recipes.name}">
-                        <h2 id="recipes_name">${recipes.name}</h2>
-                        <span class="title_aside">
-                            <svg class="clock_icon" width="30" height="30" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM10 18C5.59 18 2 14.41 2 10C2 5.59 5.59 2 10 2C14.41 2 18 5.59 18 10C18 14.41 14.41 18 10 18ZM10.5 5H9V11L14.2 14.2L15 12.9L10.5 10.2V5Z" fill="black"/>
-                                </svg>
-                            <span class="recipe_time">${recipes.time} min</span>
-                        </span>
-                        <div class="recipe">
-                            <span class="container_ingredients">
-
-                            ${recipes.ingredients.map((ingredient) => {
-                                let quantity = ingredient.quantity
-                                let unit = ingredient.unit
-                                if(quantity === undefined) {
-                                    quantity = ""
-                                }
-                                if(unit === undefined) {
-                                    unit = ""
-                                }
-                                return `<span class="ingredients">${ingredient.ingredient}:<span class="ingredients_quantities"> ${quantity}${unit}</span></span>`
-                            }).join("")}
-
-                            </span>
-                            <span class="container_description">
-                                <p class="description">${recipes.description}</p>
-                            </span>
-                        </div>
-                    </a>
-                </div>
-        `
-    })
-    
-    document.querySelector(".recipe_container").innerHTML = html
-}
-
-// Méthode fonctionnelle de recherche de recettes
+// Événement d'appel à la programmation fonctionnelle de recherche de recettes
 
 const searchInput = document.querySelector("#search")
 
-searchInput.addEventListener("input", (e) => {
-    
-    const content = e.target.value.toLowerCase()
-    const cards = document.querySelectorAll('.recipe_card')
-
-    if(content.length > 2) {
-        cards.forEach((card) => {
-            if (card.textContent.toLowerCase().includes(content)) {
-                card.style.display = "block"
-            }
-            else {
-                card.style.display = "none"
-            }
-        })
+const searchEvent = searchInput.addEventListener("input", (e) => {
+    const contentInput = e.target.value.toLowerCase()
+    if(contentInput.length > 2) {
+        return searchRecipes(contentInput)
     }
 
-    if(content.length <= 2) {
-            cards.forEach((card) => {
-                card.style.display = "block"
-            })
+    if(contentInput.length < 2) {
+        return displayRecipes(recipes)
     }
 })
+
+// Dropdowns : règles générales
+
+const dropdowns = document.querySelectorAll(".dropdown")
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector(".select")
+    const buttons = dropdown.querySelector(".dropdown_buttons")
+    const arrow = dropdown.querySelector(".arrow")
+    const menu = dropdown.querySelector(".menu")
+    const options = dropdown.querySelectorAll(".menu li")
+    
+    select.addEventListener("click", () => {
+        buttons.classList.toggle("dropdown_buttons_selected")
+        arrow.classList.toggle("arrow_rotate")
+        menu.classList.toggle("menu_open")
+    })
+})
+
+const selectIngredients = document.querySelector("#select_ingredients")
+selectIngredients.addEventListener("click", () => {
+    const dropDownIngredients = document.querySelector("#menu_ingredients")
+    dropDownIngredients.style.backgroundColor = "#3282F7"
+})
+
+const selectAppareils = document.querySelector("#select_appareils")
+selectAppareils.addEventListener("click", () => {
+    const dropDownAppareils = document.querySelector("#menu_appareils")
+    dropDownAppareils.style.backgroundColor = "#68D9A4"
+})
+
+const selectUstensils = document.querySelector("#select_ustensils")
+selectUstensils.addEventListener("click", () => {
+    const dropDownUstensils = document.querySelector("#menu_ustensils")
+    dropDownUstensils.style.backgroundColor ="#ED6454"
+})
+
+// Dropdown : règles individuelles
+
+function getIngredients(recipes) {
+    
+    recipes.forEach((recipe) => {
+        const recipesIngredients = `${recipe.ingredients.map((ingredient) => {
+        return `${ingredient.ingredient}`
+        }).join("")}`
+        
+        console.log(recipesIngredients)
+        
+    })
+    
+}
+
+// Récupérer, filtrer
+
+
+
+
+
+
+
